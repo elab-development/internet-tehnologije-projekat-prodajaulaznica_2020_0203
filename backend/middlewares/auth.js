4803c4c516948a900fe90c79f9c467f08b614306
pera <nikodijevicp01@gmail.com>
2024-01-22 21:28:47 +0100

    dodata autentifikacija

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,31 @@
+const authenticate = (req, res, next) => {
+  const userCookie = req.cookies.user;
+  const adminCookie = req.cookies.admin;
+
+  console.log(adminCookie);
+  if (userCookie == undefined && adminCookie == undefined) {
+    res.status(401).json({ message: "Not authenticated" });
+    return;
+  }
+
+  if (
+    adminCookie !== undefined &&
+    req.originalUrl.includes("karta") &&
+    req.route.methods.post == true
+  ) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+  if (
+    userCookie !== undefined &&
+    req.originalUrl.includes("dogadjaj") &&
+    req.route.methods.post == true
+  ) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  next();
+};
+
+module.exports = authenticate;
