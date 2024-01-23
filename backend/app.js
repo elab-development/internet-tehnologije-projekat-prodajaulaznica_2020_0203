5528e744148b55f42745716ea22ce58a4b399ef4
pera <nikodijevicp01@gmail.com>
2024-01-23 09:40:50 +0100

    inicijalno zavrsen back

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+require("./Repository/context");
+const adminRoutes = require("./routes/adminRoute");
+const korisnikRoutes = require("./routes/korisnikRoute");
+const dogadjajRoutes = require("./routes/dogadjajRoute");
+const kartaRoutes = require("./routes/kartaRoute");
+const tipKarteRoutes = require("./routes/tipKarteRoute");
+const path = require("path");
+
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -12,12 +20,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 5001;
+app.use("/", adminRoutes);
+app.use("/", korisnikRoutes);
+app.use("/", tipKarteRoutes);
 
-app.get("/", (req, res) => {
-  res.send("nista");
-});
+app.use("/", dogadjajRoutes);
+app.use("/", kartaRoutes);
+
+const PORT = process.env.PORT || 5000;
+const { ticketQueue, processTicketQueue } = require("./middlewares/queue");
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+  processTicketQueue();
 });
